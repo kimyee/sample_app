510579740dbea63a7ace25e700566a4ee65582d3
kimyee <kimyee.16@gmail.com>
2013-12-29 12:50:46 -0500

    Create sample app without test directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '1f8064fb5bfa71b9eb223e1e9f380d0f835f425360ec74d88d46f0e1e48dc07dcc0ec8ae8d0e128e1555ba2cb1d38e92465a709f340dc91f56cf1888d7147587'
